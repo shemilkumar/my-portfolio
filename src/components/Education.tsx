3550672dfecbc97db2404ebac349ee713089db39
shemilkumar <ekaravindhakshan@gmail.com>
2023-04-29 19:27:40 +0530

    About page completed

---
+++ src/components/Education.tsx
@@ -0,0 +1,57 @@
+import { FC, useRef } from "react";
+import Headline from "./ui/Headline";
+import ProfessionalDetails from "./ProfessionalDetails";
+import { motion, useScroll } from "framer-motion";
+
+interface EducationProps {}
+
+const Education: FC<EducationProps> = ({}) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
+  return (
+    <article className="w-full mb-72">
+      <div className="flex justify-center my-24">
+        <Headline title="Education" />
+      </div>
+
+      <div ref={ref} className="relative w-[60%] mx-auto mb-20">
+        <motion.div
+          style={{ scaleY: scrollYProgress }}
+          className="absolute w-[4px] bg-dark dark:bg-light h-full left-9 top-0 origin-top
+          md:w-[4px] md:left-[29px] xs:left-[19px]"
+        />
+        <ul className="flex flex-col items-start w-full gap-8 ml-4">
+          <ProfessionalDetails
+            company="Master of Computer Science"
+            position="Jain University"
+            time="2021 - Present"
+            place="Bangalore"
+            info={`Relevant courses included Data Structures and Algorithms, Computer Systems Engineering, and Programming languages.`}
+          />
+
+          <ProfessionalDetails
+            company="Bachelor of Computer Science"
+            position="Calicut University"
+            time="2018 - 2021"
+            place="Thrissur, Kerala"
+            info={`Completed a Bachelor's project on native Android, developed a Pothole finder mobile application using Android studio and Firebase Realtime database`}
+          />
+
+          <ProfessionalDetails
+            company="Higher Secondary"
+            position="VBHSS Thrissur"
+            time="2016 - 2018"
+            place="Thrissur, Kerala"
+            info={`Syllabus included Fundamentals of programming using C++, RDMS Basics and Web development`}
+          />
+        </ul>
+      </div>
+    </article>
+  );
+};
+
+export default Education;
---
+++ src/components/Experience.tsx
@@ -0,0 +1,51 @@
+import { FC, useRef } from "react";
+import Headline from "./ui/Headline";
+import ProfessionalDetails from "./ProfessionalDetails";
+import { motion, useScroll } from "framer-motion";
+
+interface ExperienceProps {}
+
+const Experience: FC<ExperienceProps> = ({}) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start end", "center start"],
+  });
+
+  return (
+    <article className="w-full">
+      <div className="flex justify-center mt-32 mb-20">
+        <Headline title="Experience" />
+      </div>
+
+      <div ref={ref} className="relative w-[60%] mx-auto mb-20">
+        <motion.div
+          style={{ scaleY: scrollYProgress }}
+          className="absolute w-[4px] bg-dark dark:bg-light h-full left-9 top-0 origin-top
+          md:w-[4px] md:left-[29px] xs:left-[19px]"
+        />
+
+        <ul className="flex flex-col items-start w-full ml-4">
+          <ProfessionalDetails
+            company="TCS"
+            position="Programmer"
+            time="2022 - Present"
+            place="New Delhi"
+            info={`Worked on a team responsible for developing a new mobile app feature that allowed users to create and share short-form video content, including designing and implementing a new user interface and developing the backend infrastructure to support the feature.`}
+            link="https://www.tcs.com"
+          />
+
+          <ProfessionalDetails
+            position="Graduate Trainee"
+            time="2021 - 2022"
+            place="New Delhi"
+            info={`Worked on a team responsible for developing a new mobile app feature that allowed users to create and share short-form video content, including designing and implementing a new user interface and developing the backend infrastructure to support the feature.`}
+            link="www.tcs.com"
+          />
+        </ul>
+      </div>
+    </article>
+  );
+};
+
+export default Experience;
