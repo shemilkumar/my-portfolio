f05b8b7f457d026df72b7fda610e66edbb9e2406
shemilkumar <ekaravindhakshan@gmail.com>
2023-04-30 16:31:55 +0530

    Animation added in contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,6 +2,8 @@
 import SEO from "@/components/SEO";
 import { GithubIcon, LinkedInIcon } from "@/components/assests/Icons";
 import Headline from "@/components/ui/Headline";
+import { container, item } from "@/util/staggeredAnimation";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { FC } from "react";
@@ -13,72 +15,88 @@ const contact: FC<contactProps> = ({}) => {
     <>
       <SEO pageTitle="Contact | Shemil" pageDescription="" />
       <main className="flex flex-col items-center justify-center w-full">
-        <div className="grid w-[85%] grid-cols-12 pt-56 gap-y-5">
-          <div className="col-span-12 pb-4 border-b-2 border-dark">
+        <motion.div
+          className="grid w-[85%] grid-cols-12 pt-56 gap-y-5"
+          variants={container}
+          initial="hidden"
+          animate="show"
+        >
+          <motion.div
+            className="col-span-12 pb-4 border-b-2 border-dark"
+            variants={item}
+          >
             <Headline title="Contact" />
-          </div>
+          </motion.div>
 
           <div className="col-span-6">
-            <div className="flex items-center gap-12">
+            <motion.div className="flex items-center gap-12" variants={item}>
               <h2 className="text-xl font-semibold uppercase">Mail</h2>
               <ul className="flex flex-col gap-2 text-base font-medium text-dark/75">
-                <Link
-                  href="mailto:shemil0055@gmail.com"
-                  className="flex items-center gap-2 "
-                >
-                  <img
-                    alt="mail"
-                    className="w-8 h-auto"
-                    src={`https://img.icons8.com/fluency/48/null/mail.png`}
-                  />
-                  <span>shemil0055@gmail.com</span>
-                </Link>
-              </ul>
-            </div>
-          </div>
-          <div className="col-span-6 ">
-            <div className="flex gap-12">
-              <h2 className="text-xl font-semibold uppercase">Social medias</h2>
-              <ul className="flex flex-col gap-2 text-base font-medium text-dark/75">
-                <Link
-                  href="https://linkedin.com/in/shemilkumar"
-                  target="_blank"
-                  className="flex items-center gap-2 "
-                >
-                  <span className="p-0.5 w-7">
-                    <LinkedInIcon />
-                  </span>
-                  <span>linkedin</span>
-                </Link>
-                <Link
-                  href="https://github.com/shemilkumar"
-                  target="_blank"
-                  className="flex items-center gap-2 "
-                >
-                  <span className="w-7">
-                    <GithubIcon />
-                  </span>
-                  <span>github</span>
-                </Link>
-                <Link
-                  href="https://www.instagram.com/__.shemil._/"
-                  target="_blank"
-                  className="flex items-center gap-2 "
-                >
-                  <span className="w-7">
+                <motion.li>
+                  <Link
+                    href="mailto:shemil0055@gmail.com"
+                    className="flex items-center gap-2 "
+                  >
                     <img
                       alt="mail"
-                      className="w-full h-auto"
-                      src={`https://img.icons8.com/fluency/48/null/instagram-new.png`}
+                      className="w-8 h-auto"
+                      src={`https://img.icons8.com/fluency/48/null/mail.png`}
                     />
-                  </span>
-
-                  <span>instagram</span>
-                </Link>
+                    <span>shemil0055@gmail.com</span>
+                  </Link>
+                </motion.li>
               </ul>
-            </div>
+            </motion.div>
           </div>
-        </div>
+          <div className="col-span-6 ">
+            <motion.div className="flex gap-12" variants={container}>
+              <h2 className="text-xl font-semibold uppercase">Social medias</h2>
+              <ul className="flex flex-col gap-2 text-base font-medium text-dark/75">
+                <motion.li variants={item}>
+                  <Link
+                    href="https://linkedin.com/in/shemilkumar"
+                    target="_blank"
+                    className="flex items-center gap-2 "
+                  >
+                    <span className="p-0.5 w-7">
+                      <LinkedInIcon />
+                    </span>
+                    <span>linkedin</span>
+                  </Link>
+                </motion.li>
+                <motion.li variants={item}>
+                  <Link
+                    href="https://github.com/shemilkumar"
+                    target="_blank"
+                    className="flex items-center gap-2 "
+                  >
+                    <span className="w-7">
+                      <GithubIcon />
+                    </span>
+                    <span>github</span>
+                  </Link>
+                </motion.li>
+                <motion.li variants={item}>
+                  <Link
+                    href="https://www.instagram.com/__.shemil._/"
+                    target="_blank"
+                    className="flex items-center gap-2 "
+                  >
+                    <span className="w-7">
+                      <img
+                        alt="mail"
+                        className="w-full h-auto"
+                        src={`https://img.icons8.com/fluency/48/null/instagram-new.png`}
+                      />
+                    </span>
+
+                    <span>instagram</span>
+                  </Link>
+                </motion.li>
+              </ul>
+            </motion.div>
+          </div>
+        </motion.div>
       </main>
     </>
   );
---
+++ src/util/staggeredAnimation.ts
@@ -0,0 +1,29 @@
+export const slowContainer = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+export const slowItem = {
+  hidden: { opacity: 0, y: "-50px" },
+  show: { opacity: 1, y: "0px", transition: { duration: 1 } },
+};
+
+export const container = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+export const item = {
+  hidden: { opacity: 0, y: "-50px" },
+  show: { opacity: 1, y: "0px", transition: { duration: 0.5 } },
+};
