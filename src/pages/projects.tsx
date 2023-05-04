8f3e6248d6ee8a527c0613fa533e0561eb0bbcd4
shemilkumar <ekaravindhakshan@gmail.com>
2023-05-04 21:14:07 +0530

    Added hover animation in projects

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,5 +1,7 @@
-import Headline, { HeadlineAnimated } from "@/components/ui/Headline";
 import { FC } from "react";
+import { HeadlineAnimated } from "@/components/ui/Headline";
+import { fastContainer, fastItem } from "@/util/staggeredAnimation";
+
 import StayHubImage from "../../public/img/StayHub-min.png";
 import ShopspotImage from "../../public/img/Shopspot-min.png";
 import BlackJackImage from "../../public/img/BlackJack-min.png";
@@ -22,7 +24,34 @@ interface ProjectProps {
   link: string;
   type: string;
   summary?: string;
+  technologies?: string[];
 }
+
+const SkillSetListing = ({ technologies }: { technologies?: string[] }) => {
+  return (
+    <motion.div
+      className="absolute flex flex-wrap items-center justify-center w-full h-full p-12 bg-dark/70"
+      variants={fastContainer}
+      initial="hidden"
+      whileHover="show"
+    >
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        {technologies &&
+          technologies.map((technology, i) => {
+            return (
+              <motion.span
+                key={i}
+                className="px-2 py-1 text-sm font-semibold rounded-lg text-light bg-gradient-to-r from-blue-700 to-purple-700"
+                variants={fastItem}
+              >
+                {technology}s
+              </motion.span>
+            );
+          })}
+      </div>
+    </motion.div>
+  );
+};
 
 const FeaturedProject: FC<ProjectProps> = ({
   image,
@@ -31,14 +60,19 @@ const FeaturedProject: FC<ProjectProps> = ({
   summary,
   github,
   link,
+  technologies,
 }) => {
   return (
     <motion.article className="flex items-center justify-center w-full gap-8 p-12 border border-solid shadow-2xl xl:py-16 xl:flex-col border-dark/25 dark:shadow-purple-300 rounded-xl md:p-8 xs:p-4 sm:gap-4 md:shadow-lg">
-      <Image
-        alt="project"
-        src={image}
-        className="w-1/2 xl:w-[90%] md:w-full rounded-lg"
-      />
+      <Link
+        href={link}
+        target="_blank"
+        className="relative w-1/2 xl:w-[90%] md:w-full"
+      >
+        <SkillSetListing technologies={technologies} />
+        <Image alt="project" src={image} className="w-full rounded-lg" />
+      </Link>
+
       <div className="flex flex-col items-start justify-between w-1/2 xl:w-[90%] md:w-full gap-4 sm:gap-2">
         <span className="text-xl font-semibold text-transparent bg-purple-600 bg-gradient-to-tr from-purple-600 to-blue-600 bg-clip-text lg:text-lg">
           {type}
@@ -59,10 +93,24 @@ const FeaturedProject: FC<ProjectProps> = ({
   );
 };
 
-const Project: FC<ProjectProps> = ({ image, title, type, github, link }) => {
+const Project: FC<ProjectProps> = ({
+  image,
+  title,
+  type,
+  github,
+  link,
+  technologies,
+}) => {
   return (
     <article className="flex flex-col items-center justify-center w-full gap-8 p-6 border border-solid shadow-2xl dark:shadow-b-lg rounded-2xl dark:shadow-purple-300 xs:p-4 md:shadow-lg">
-      <Image alt="project" src={image} className="w-full rounded-lg " />
+      <Link
+        href={link}
+        target="_blank"
+        className="relative w-full xl:w-[90%] md:w-full"
+      >
+        <SkillSetListing technologies={technologies} />
+        <Image alt="project" src={image} className="w-full rounded-lg " />
+      </Link>
       <div className="flex flex-col items-start justify-between w-full gap-4">
         <span className="text-xl font-semibold text-transparent bg-purple-600 bg-gradient-to-tr from-purple-600 to-blue-600 bg-clip-text lg:text-lg">
           {type}
@@ -112,18 +160,23 @@ const Projects: FC<ProjectsProps> = ({}) => {
               summary={`A feature-rich online Home booking web applicationb built using MERN stack. The backend of this application was developed using Express in Node.js, with MongoDB for database management. The frontend, on the other hand, was developed using React.js, TailwindCSS, and Redux, providing a better user interface with reusable components. Both the frontend and backend were fully written in TypeScript, ensuring type safety and code clarity.`}
               github="https://github.com/shemilkumar/StayHub"
               link="https://stayhub.shemilkumar.com"
+              technologies={[
+                "Typescript",
+                "Node.js",
+                "React.js",
+                "Redux",
+                "Express",
+                "MongoDB",
+                "REST API",
+                "JWT",
+                "AWS",
+              ]}
             />
           </motion.div>
 
           <div className="flex col-span-12 gap-20 xl:gap-12 lg:gap-6 md:flex-col">
             <motion.div
               className=""
-              // initial={{ x: -400, opacity: 0 }}
-              // whileInView={{
-              //   x: 0,
-              //   opacity: 1,
-              //   transition: { duration: 0.6 },
-              // }}
               initial={{ scale: 0.2, opacity: 0 }}
               whileInView={{
                 scale: 1,
@@ -138,16 +191,11 @@ const Projects: FC<ProjectsProps> = ({}) => {
                 title="Netflix clone"
                 github="https://github.com/shemilkumar/netflix-clone"
                 link="http://netflix-clone-5d65d.web.app/"
+                technologies={["React.js", "Firebase", "CSS", "Git", "API"]}
               />
             </motion.div>
             <motion.div
               className=""
-              // initial={{ x: -400, opacity: 0 }}
-              // whileInView={{
-              //   x: 0,
-              //   opacity: 1,
-              //   transition: { duration: 0.6 },
-              // }}
               initial={{ scale: 0.2, opacity: 0 }}
               whileInView={{
                 scale: 1,
@@ -162,6 +210,13 @@ const Projects: FC<ProjectsProps> = ({}) => {
                 title="BlackJack - Online Web Game"
                 github="https://github.com/shemilkumar/BlackJack-webgame"
                 link="https://blackjack-webgame.netlify.app/"
+                technologies={[
+                  "Javascript",
+                  "ES06",
+                  "CSS & HTML",
+                  "Git",
+                  "Parcel",
+                ]}
               />
             </motion.div>
           </div>
@@ -183,18 +238,21 @@ const Projects: FC<ProjectsProps> = ({}) => {
               summary={`A Stunning E-commerce Website built using ReactJS, Redux,React Router, TailwindCSS and Firebase. It offers advanced features such as product browsing, search, filtering and sorting, shopping cart, and user account creation. The project also boasts a responsive design, dark mode, and integration with the dummyjson API. It showcases my ability to deliver high-quality web applications that meet and exceed user expectations.`}
               github="https://github.com/shemilkumar/shopspot"
               link="https://shopspot-shemil.vercel.app/"
+              technologies={[
+                "React.js",
+                "Redux",
+                "TailwindCSS",
+                "Firebase",
+                "Javascript",
+                "Git",
+                "API",
+              ]}
             />
           </motion.div>
 
           <div className="flex col-span-12 gap-20 xl:gap-12 lg:gap-6 md:flex-col">
             <motion.div
               className=""
-              // initial={{ x: -400, opacity: 0 }}
-              // whileInView={{
-              //   x: 0,
-              //   opacity: 1,
-              //   transition: { duration: 0.7 },
-              // }}
               initial={{ scale: 0.2, opacity: 0 }}
               whileInView={{
                 scale: 1,
@@ -209,19 +267,17 @@ const Projects: FC<ProjectsProps> = ({}) => {
                 title="Forkify - Food Recipe Website"
                 github="https://github.com/shemilkumar/forkify"
                 link="https://forkify-shemil.netlify.app"
+                technologies={[
+                  "Vanila Javascript",
+                  "CSS & HTML",
+                  "MVC",
+                  "Git",
+                  "API",
+                ]}
               />
             </motion.div>
             <motion.div
               className=""
-              // initial={{
-              //   x: -400,
-              //   opacity: 0,
-              // }}
-              // whileInView={{
-              //   x: 0,
-              //   opacity: 1,
-              //   transition: { duration: 0.7 },
-              // }}
               initial={{ scale: 0.2, opacity: 0 }}
               whileInView={{
                 scale: 1,
@@ -236,6 +292,13 @@ const Projects: FC<ProjectsProps> = ({}) => {
                 title="Natours - Tour Booking Website"
                 github="https://github.com/shemilkumar/natours"
                 link="http://natours-shemil.cyclic.app/"
+                technologies={[
+                  "Node.js",
+                  "Express",
+                  "MongoDB",
+                  "REST API",
+                  "PUG",
+                ]}
               />
             </motion.div>
           </div>
--- src/util/staggeredAnimation.ts
+++ src/util/staggeredAnimation.ts
@@ -27,3 +27,18 @@ export const item = {
   hidden: { opacity: 0, y: "-50px" },
   show: { opacity: 1, y: "0px", transition: { duration: 0.5 } },
 };
+
+export const fastContainer = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+export const fastItem = {
+  hidden: { opacity: 0, y: "-50px" },
+  show: { opacity: 1, y: "0px", transition: { duration: 0.4 } },
+};
